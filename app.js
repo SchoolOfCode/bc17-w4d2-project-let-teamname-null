987c1b85b7e85b56a137163e7f1a051930fc5a2b
Aidan Isbell <IOLV@Outlook.com>
2024-07-23 14:05:17 +0100

    User Story 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-Test
+import express from 'express';
+const app = express();
+const port = 8080;
+
+
+app.get (`/`, (req, res) => {
+    res.status(200).send('Hello World!')
+})
+
+app.listen(port, () => 
+    console.log(`Listening to port at ${port}`)
+)
