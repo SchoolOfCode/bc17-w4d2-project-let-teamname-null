7cf67bfc5607081e9045d0085edc64251a6d78e8
Aidan Isbell <IOLV@Outlook.com>
2024-07-24 12:07:30 +0100

    User Story 2 - Helmet

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import helmet from 'helmet';
+
 const app = express();
 const port = 8080;
 
+app.use(helmet());
 
 app.get (`/`, (req, res) => {
-    res.status(200).send('Hello World!')
+    res.status(200).send('Hello World!').getHeaders
 })
 
 app.listen(port, () => 
